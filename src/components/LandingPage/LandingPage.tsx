bba99dca70f23e2843d84bff6a3ab8c12b7432c4
egyptofrance <egyptofrance@github.com>
2025-10-24 08:53:29 -0400

    fix: Remove duplicate Footer from landing page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,15 +1,12 @@
-import { Footer } from "./Footer";
 import HeroSection from "./HeroSection";
 import CTA from "./cta";
 
 export const LandingPage = () => {
   return (
-    <div>
-      <div className="flex flex-col gap-16">
-        <HeroSection />
-        <CTA />
-      </div>
-      <Footer />
+    <div className="flex flex-col gap-16">
+      <HeroSection />
+      <CTA />
     </div>
   );
 };
+
