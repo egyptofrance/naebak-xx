e2cc79b762089259fcac169063e947cab14e68af
egyptofrance <egyptofrance@github.com>
2025-10-24 08:58:16 -0400

    feat: Remove CTA section from landing page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,11 +1,9 @@
 import HeroSection from "./HeroSection";
-import CTA from "./cta";
 
 export const LandingPage = () => {
   return (
     <div className="flex flex-col gap-16">
       <HeroSection />
-      <CTA />
     </div>
   );
 };
