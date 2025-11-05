607dbb5aed4592c178775b8896362336f459e12b
ahmed aboeldahab <alcounsol@gmail.com>
2025-11-05 14:18:20 -0500

    feat: إزالة زر Introducing من Hero Section

--- src/components/LandingPage/HeroSection.tsx
+++ src/components/LandingPage/HeroSection.tsx
@@ -10,15 +10,7 @@ export default function HeroSection() {
       <section className="py-10 lg:py-20 text-center px-6 max-w-5xl mx-auto" dir="rtl">
       <div className="flex flex-col gap-10 w-full">
         <div className="space-y-4 flex flex-col items-center flex-1">
-          <Link href={"#"}>
-            <div className="flex items-center  gap-2 py-1 px-3 w-fit rounded-full border border-border dark:border-none bg-secondary">
-              <Sparkles size={16} />
-              <span className="text-md font-medium lg:text-base">
-                Introducing
-              </span>
-              <ArrowRight size={16} />
-            </div>
-          </Link>
+
           <h1 className="font-semibold text-3xl lg:text-5xl">{t("title")}</h1>
           <p className="text-muted-foreground leading-loose lg:text-lg lg:leading-relaxed max-w-4xl">
             {t("description")}
